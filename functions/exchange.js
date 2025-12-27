4dff180714b25fa36eb5be56f9da919e0ae8b240
srikarb54 <srikarb54@gmail.com>
2025-12-27 03:06:17 -0600

    first commit

--- functions/exchange.js
+++ functions/exchange.js
@@ -3,21 +3,26 @@ export async function onRequest(context) {
   const { code } = body;
 
   const client_key = "sbawu97nzqxev1whkh";
-  const client_secret = "fgs21FkmIULQBV9iFZ6FJvJKOY6dPBPL";
+  const client_secret = "fgs21FkmIULQBV9iFZ6FJKOY6dPBPL";
+
+  // Build x-www-form-urlencoded body
+  const form = new URLSearchParams();
+  form.append("client_key", client_key);
+  form.append("client_secret", client_secret);
+  form.append("code", decodeURIComponent(code));
+  form.append("grant_type", "authorization_code");
+  form.append("redirect_uri", "https://postik5987.pages.dev/callback");
 
   const resp = await fetch("https://open.tiktokapis.com/v2/oauth/token/", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      client_key,
-      client_secret,
-      code,
-      grant_type: "authorization_code",
-      redirect_uri: "https://postik5987.pages.dev/callback"
-    })
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: form.toString()
   });
 
   const data = await resp.json();
+
   return new Response(JSON.stringify(data), {
     headers: { "Content-Type": "application/json" }
   });
