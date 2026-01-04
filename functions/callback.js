f1f56a90da5f1d3fe515be5c8a81967479be5744
srikarb54 <srikarb54@gmail.com>
2026-01-04 03:22:04 -0600

    first commit

--- functions/callback.js
+++ functions/callback.js
@@ -2,7 +2,6 @@ export async function onRequest(context) {
   try {
     const url = new URL(context.request.url);
     const code = url.searchParams.get("code");
-    const state = url.searchParams.get("state");
 
     if (!code) {
       return new Response("Error: TikTok did not return code", { status: 400 });
@@ -11,7 +10,6 @@ export async function onRequest(context) {
     const client_key = "awxz5bpprf0w1e7t";
     const client_secret = "c3506efI0LsvQNmFfnuqlUqJ4saDOEt2";
 
-    // Build x-www-form-urlencoded body
     const body = new URLSearchParams();
     body.append("client_key", client_key);
     body.append("client_secret", client_secret);
@@ -21,20 +19,20 @@ export async function onRequest(context) {
 
     const resp = await fetch("https://open.tiktokapis.com/v2/oauth/token/", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: body.toString(),
     });
 
     const data = await resp.json();
 
-    // Display results as HTML for debugging
     return new Response(`
-	<script>
-	localStorage.setItem("access_token", "${data.access_token}");
-	window.location.href = "/publish.html";
-	</script>
-	`, { headers: { "Content-Type": "text/html" }});
+      <script>
+        localStorage.setItem("access_token", "${data.access_token}");
+        window.location.href = "/publish.html";
+      </script>
+    `, { headers: { "Content-Type": "text/html" } });
+
+  } catch (err) {
+    return new Response("Callback error: " + err.toString(), { status: 500 });
   }
 }
